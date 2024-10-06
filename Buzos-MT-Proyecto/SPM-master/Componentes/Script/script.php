591160c97d81fd858f214e9efb3a8ebef6328f27
Yonathan Nieves <yonathannieves17@gmail.com>
2024-10-05 19:40:13 -0500

    Corrección del Scroll.

--- Buzos-MT-Proyecto/SPM-master/Componentes/Script/script.php
+++ Buzos-MT-Proyecto/SPM-master/Componentes/Script/script.php
@@ -8,7 +8,7 @@
 	<script src="../js/bootstrap.min.js"></script>
 
 	<!-- jQuery Custom Content Scroller V3.1.5 -->
-	<!-- <script src="../js/jquery.mCustomScrollbar.concat.min.js"></script> -->
+	<script src="../js/jquery.mCustomScrollbar.concat.min.js"></script>
 
 	<!-- Bootstrap Material Design V4.0 -->
 	<!-- <script src="../js/bootstrap-material-design.min.js"></script>
