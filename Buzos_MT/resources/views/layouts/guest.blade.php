22950e1d1152876647a8bd39a04e2392aae84863
Jose Guerrero <josedanielguerreromolina098@gmail.com>
2024-12-09 11:54:15 -0500

    Ajuste de posicionamiento de logo

--- Buzos_MT/resources/views/layouts/guest.blade.php
+++ Buzos_MT/resources/views/layouts/guest.blade.php
@@ -27,12 +27,12 @@
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div>
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+        <a href="/">
+        <img src="{{ asset('Logo/logo.png') }}" alt="Logo" class="logo" width="140px">
+    </a>
         </div>
 
-        <div class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-lg mt-6 px-6 py-2 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
