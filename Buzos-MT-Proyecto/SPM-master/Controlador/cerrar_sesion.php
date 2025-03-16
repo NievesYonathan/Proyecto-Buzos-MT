8330575ba0144edd0ef2b4319ad382dbba4d782b
Harol-d <harodgomez2007@gmail.com>
2025-03-16 01:46:00 -0500

    New breach for dev AppMobile

--- Buzos-MT-Proyecto/SPM-master/Controlador/cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../Login-Registro/config.php');
-
-//Reset OAuth access token
-$google_client->revokeToken();
-
-session_destroy();
-
-header("Location:../Login-Registro/login.php");
