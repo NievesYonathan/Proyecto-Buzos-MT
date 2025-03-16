91ee459e8d3f2eaf80a788d71ff6567cd05cc6c8
Harol-d <harodgomez2007@gmail.com>
2025-03-16 02:43:40 -0500

    Creacion de los archivos correspondientes

--- buzosmt/lib/Presentation/Login.dart
+++ buzosmt/lib/Presentation/Login.dart
@@ -5,6 +5,6 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar(title: Text('Login')));
   }
 }
--- buzosmt/lib/main.dart
+++ buzosmt/lib/main.dart
@@ -11,6 +11,7 @@ class BuzosMt extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
