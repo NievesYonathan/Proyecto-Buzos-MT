dbac2aa7e24fc002fc6df168992f36da97a07258
Harol-d <harodgomez2007@gmail.com>
2025-03-16 03:54:08 -0500

    Implementa pantalla de inicio de sesión (Basica)

--- buzosmt/lib/Presentation/Login.dart
+++ buzosmt/lib/Presentation/Login.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  String email = '';
+  String password = '';
+
+  login(String email, String password) {
+    print('Email: $email');
+    print('Password: $password');
+     
+    }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Login')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+          ),
+          ListTile(
+            title: TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Contaseña'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          login(emailController.text, passwordController.text);
+        },
+        child: Text('ingresar'),
+      ),
+    );
   }
 }
