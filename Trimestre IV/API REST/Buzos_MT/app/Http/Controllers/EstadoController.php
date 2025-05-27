6f496589aa8258b06c4e53ec5dcf96e5f2a100ae
Yonathan Nieves <yonathannieves17@gmail.com>
2025-05-27 15:28:42 -0500

    Ajuste de Controladores - Ajuste .yml.

--- Trimestre IV/API REST/Buzos_MT/app/Http/Controllers/EstadoController.php
+++ Trimestre IV/API REST/Buzos_MT/app/Http/Controllers/EstadoController.php
@@ -13,7 +13,7 @@ class EstadoController extends Controller
     public function __construct()
     {
         // Configuración de la API base
-        $this->apiBase = 'http://localhost/Proyecto-Buzos-MT/Buzos_MT/public/api';
+        $this->apiBase = 'http://localhost/dash/Proyecto-Buzos-MT/Trimestre IV/API REST/Buzos_MT/public/api';
     }
 
     // Obtener todos los estados
--- Trimestre IV/API REST/Buzos_MT/app/Http/Controllers/ListaCargoController.php
+++ Trimestre IV/API REST/Buzos_MT/app/Http/Controllers/ListaCargoController.php
@@ -12,7 +12,7 @@ class ListaCargoController extends Controller
 
     public function __construct()
     {
-        $this->apiBase = 'http://localhost/Proyecto-Buzos-MT/Buzos_MT/public/api';
+        $this->apiBase = 'http://localhost/dash/Proyecto-Buzos-MT/Trimestre IV/API REST/Buzos_MT/public/api';
         Http::timeout(5);
     }
 
